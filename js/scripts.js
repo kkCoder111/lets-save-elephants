8c60882d7f2bfdda8ac73bf616854c18bad926c4
kkCoder111 <90219573+kkCoder111@users.noreply.github.com>
2021-09-25 14:29:39 -0700

    Adding JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,56 @@
-function help() {
-	alert("Click on an image to zoom in and enter the image viewer. Click an image while in the image viewer to view the original source.");
+//Source: https://www.wwf.org.uk/learn/fascinating-facts/elephants
 
+var facts = [
+	"Elephants are the largest land mammal.",
+	"You can tell African and Asian elephants apart by their ears.",
+	"Elephants have 150,000 muscles units in their trunk.",
+	"They have thick skin.",
+	"They constantly eat.",
+	"Calves can stand within 20 minutes of birth."
+]
+
+function random(){
+	let display = document.getElementById("fact");
+	console.log(display);
+	let result = Math.floor(Math.random() * 5) + 1;
+
+	console.log(result);
+	display.innerHTML = facts[result];
 }
+
+function setCookie(name, value, days) {
+	const dateHandler = new Date();
+	let multiplier = 24*60*60*1000;
+	dateHandler.setTime(dateHandler.getTime() + (days*multiplier))
+	let expiryDate = "expires=" + dateHandler.toGMTString();
+	document.cookie = name + "=" + value + ";" + expiryDate	+ ";path=/";
+}
+
+function getCookie(name) {
+	let cname = name + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(";");
+	for(let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == " ") {
+			c = c.substring(1);
+		}
+		if (c.indexOf(cname) == 0) {
+			return c.substring(cname.length, c.length);
+		}
+	}
+	return "";
+}
+
+function checkCookie(name, valueIfNotFound, daysIfNotFound) {
+	let cookie = getCookie(name);
+	console.log(cookie);
+	if (cookie != ""){
+		return cookie;
+	}
+	else {
+		setCookie(name, valueIfNotFound, daysIfNotFound);
+	}
+}
+
+console.log(facts);
